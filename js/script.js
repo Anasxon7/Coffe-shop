94a356141b9ceb2fe4d3d958069ab79660e6d1c5
Madaminov <muhammadanasxon6694@gmail.com>
2022-11-22 16:22:27 +0500

    add

--- js/script.js
+++ js/script.js
@@ -19,7 +19,7 @@ let cartItem = document.querySelector('.cart-items-container');
 document.querySelector('#cart-btn').onclick = () =>{
     cartItem.classList.toggle('active');
     navbar.classList.remove('active');
-    searchForm.classList.remove('active');
+    searchForm.classList.remove('active'); 
 }
 
 window.onscroll = () =>{
